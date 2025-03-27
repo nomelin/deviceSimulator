a2a6f2e96a25dba74248a41f0254224bc8a51f0d
nomelin <2023686595@qq.com>
2025-03-27 14:53:19 +0800

    0.12

--- start.bat
+++ start.bat
@@ -1,8 +1,15 @@
 @echo off
 chcp 65001
 
-echo 正在启动 device 模拟器...
-java -Dfile.encoding=UTF-8 -jar target\device-1.0.jar
+setlocal enabledelayedexpansion
 
+:: 获取脚本所在目录
+set "SCRIPT_DIR=%~dp0"
+
+:: 指定配置文件路径
+set "CONFIG_FILE=%SCRIPT_DIR%config.yml"
+
+echo 正在启动 device 模拟器...
+java -Dfile.encoding=UTF-8 -jar "%SCRIPT_DIR%target\device-1.0.jar" --spring.config.location="file:!CONFIG_FILE!"
 
 pause
